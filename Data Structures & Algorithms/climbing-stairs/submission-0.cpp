566135721ed6f2b518e33b03fa846a597df0e517
mukut02 <chatterjeemukut@gmail.com>
2026-05-30 02:19:35 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,14 @@
+#include <cstring>
+class Solution {
+public:
+int dp[46];
+int f(int i){
+    if(dp[i]!=-1) return dp[i];
+    if(i<=1) return dp[i]=1;
+    return dp[i]=f(i-1) + f(i-2);
+}
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+        return f(n);
+    }
+};
