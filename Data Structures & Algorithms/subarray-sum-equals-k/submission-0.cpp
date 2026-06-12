47c1fe308bb1e39d87c87a67febad94a27923524
mukut02 <chatterjeemukut@gmail.com>
2026-06-12 17:47:06 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int,int> mpp;
+        int sum=0;
+        mpp[0]=1;
+        int ans=0;
+        for(auto &x : nums){
+            sum+=x;
+            ans+=mpp[sum-k];
+            mpp[sum]++;
+        }
+        return ans;
+    }
+};
